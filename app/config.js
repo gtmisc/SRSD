186496b8aa8aeb34b3b45d6cc824d988fe840b3f
Sukitti (Troy) Punak <Sukitti.Punak@thomsonreuters.com>
2016-12-16 15:38:54 +0700

    v0.0.1

--- app/config.js
+++ app/config.js
@@ -4,6 +4,6 @@ var config = {
 	db: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/nasdaq',
   // db: process.env.MONGOLAB_URI || 'mongodb://usra:usra1@ds127968.mlab.com:27968/atp',
 	test_db: 'mongodb://localhost:27017/nasdaq_test',
-	intervalTimeToGetData: 30000
+	intervalTimeToGetData: 60000
 }
 module.exports = config;
